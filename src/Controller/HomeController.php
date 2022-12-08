b1a1f39c703232185b31a1a6895c72c2700d650f
nico <nico@notmymail.com>
2022-12-08 10:14:28 +0100

    adding result table in test tls

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ServerInfoRepository;
+use App\Repository\VhostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ServerInfoRepository $serverRepo, VhostRepository $vhostRepo): Response
     {
+        $servers = $serverRepo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'servers' => $servers,
         ]);
     }
 }
--- src/Controller/TestScriptController.php
+++ src/Controller/TestScriptController.php
@@ -2,30 +2,52 @@
 
 namespace App\Controller;
 
+use App\Form\TestVhostType;
 use App\Kernel;
 use App\Service\ServerInfoService;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *  test a given domain and get  infos on tls certificate 
+ */
 class TestScriptController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    public function index( ServerInfoService $serverInfo ): Response
+    public function index( Request $request, ServerInfoService $serverInfo ): Response
     {
-        $domain="www.pointvirgule.net";
+        $tlsData = null;
 
-        try {
-            $tlsData =  $serverInfo->getTlsCert($domain);
-            dd($tlsData);
-        } catch(Exception $e) {
-            dd($e);
+        //add form
+        $testVhostForm = $this->createFormBuilder()
+            ->add('testVhost', TestVhostType::class)
+            ->add('addbtn',SubmitType::class, [
+                'label' => 'valider'
+            ])
+            ->getForm();
+
+        $testVhostForm->handleRequest($request);
+
+        if ($testVhostForm->isSubmitted() && $testVhostForm->isValid()) {
+            try {
+                $domain = $testVhostForm->getData()['testVhost']['hostname'];
+                $tlsData =  json_decode(json_encode($serverInfo->getTlsCert($domain)), true);
+                // dd($tlsData);
+                // dd($serverInfo->getTlsCert($domain));
+            } catch(Exception $e) {
+                $this->addFlash('error', "impossible de récuperer les informations");
+            }
         }
 
-        return $this->render('test_script/index.html.twig', [
-            'controller_name' => 'TestScriptController',
+        return $this->renderForm('test_script/index.html.twig', [
+            'testVhostForm' => $testVhostForm,
+            'tlsData' => $tlsData
         ]);
     }
 }
